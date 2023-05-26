2ec174b28f01a9deff7aceaf8e12a83ff61e97bb
sohan-r30 <sohanakondo139@gmail.com>
2023-05-26 19:37:04 +0600

    redeploy and uri chanes

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fhpe21r.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhpe21r.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,15 +26,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     const database = client.db("superToyDB")
     const superToysCollection = database.collection("Toys")
     const ReviewCollection = database.collection("Reviews")
-
-    const indexKeys = {toyName: 1};
-    const indexOptions = {name: "ToyName"};
-
-    const result = await superToysCollection.createIndex(indexKeys,indexOptions);
 
     // route for add a toy
 
@@ -139,7 +133,7 @@ async function run() {
     })
 
     // delete a toy from my posted toys
-    app.delete("/delete-toy:id", async(req, res) => {
+    app.delete("/delete-toy/:id", async(req, res) => {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await superToysCollection.deleteOne(query);
